691c7879ee2b1696d26a0469029801e1fca435f5
Eake123 <82546019+Eake123@users.noreply.github.com>
2022-07-03 09:43:13 -0600

    Update compare.py

--- compare.py
+++ compare.py
@@ -12,30 +12,27 @@ class r:
     
     def main(self):
         voo,btc = self.normalize()
-        voo_close,btc_close = self.get_close(voo,btc)
+        voo_close = self.get_close(voo,'close')
+        btc_close = self.get_close(btc,4)
         r = numpy.corrcoef(voo_close, btc_close)[0, 1]
         return r
 
     def normalize(self):
         if len(self.voo) > len(self.btc):
             voo = self.normalize_list(self.voo,len(self.btc))
-            btc = self.normalize_list(self.btc,'False')
+            btc = self.btc
 
         else:
-            voo = self.normalize_list(self.voo,'False')
+            voo = self.voo
             btc = self.normalize_list(self.btc,len(self.voo))
         return voo,btc
     
     def normalize_list(self,candles,length):
-        if isinstance(length,int):
-            return candles[-length:]
-        else:
-            return candles
+        return candles[-length:]
     
-    def get_close(self,first,second):
-        first = [float(x['close']) for x in first]
-        second = [float(x[4]) for x in second]
-        return first,second
+    def get_close(self,candle_list,key):
+        candle_list = [float(x[key]) for x in candle_list]
+        return candle_list
         
 if __name__ == '__main__':
     m = r()
